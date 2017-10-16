a60e26aa78a350e1ed2a2b3f63a31d4713739e1f
Senya Webster <senya.webster@gmail.com>
2017-10-16 09:01:03 -0700

    add working seconds calc

--- js/space-calc.js
+++ js/space-calc.js
@@ -13,38 +13,20 @@ export class spaceAge {
   }
 
   // -->Take two dates and determine the difference, in seconds, between the two. (This way a user can enter a birthdate and the application can compare the birthdate against the current time to come up with an accurate age.)
-  getAge(dateString) {
-    let today = new Date();
-    let birthDate = new Date(dateString);
-    let age = today.getFullYear() - birthDate.getFullYear();
-    let m = today.getMonth() - birthDate.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
-    }
-    let secondsAge = age * 3.154e+7;
-    return secondsAge;
-  }
-      // ^^^ should I make a new class here or refactor to fit the constructor above?
-
   function getAge(dateString) {
     var today = new Date();
     var birthDate = new Date(dateString);
 
     var y = today.getFullYear() - birthDate.getFullYear();
-    debugger;
     var m = today.getMonth() - birthDate.getMonth();
-    var d = today.getDate() - birthdate.getDate();
-
-    var age = (y * 31536000) + (m * 2628000) + (d * 86400);
+    var d = today.getDate() - birthDate.getDate();
+    var ageInSeconds = (y * 31536000) + (m * 2628000) + (d * 86400);
 
     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
+        ageInSeconds--;
     }
-    return age;
+		return ageInSeconds;
   }
-  alert('age: ' + getAge("1980/08/10"));
-    // The code above is for assessing someone's age in seconds and excluding non-aged people (with birthdates in the future), but I haven't been able to get d to be defined. I can run the calculations the variable represents in jsFIDDLE no problem, but the variable still isn't defined. https://jsfiddle.net/jb8zkzj6/
-
 
   // -->Return the age of a human in Mercury years. (A Mercury year is .24 Earth years.)
 
